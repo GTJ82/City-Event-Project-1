5ca8217f68b5fe0da06690d1ec73924a16b5edac
Randall Phillips <rphill45@uncc.edu>
2019-11-04 08:03:51 -0500

    corrected conflicts

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-var city = "charlotte";
+$.ajax({
+    url: "https://api.predicthq.com/v1/places?&q=" + city,
+    method: "GET",
+    headers: {
+      "Authorization": "Bearer TV3nvxQ22JQnqlWiIJzMa3wvh5wRBwCwMHUqdueb"
+    }
+  }).then(function (response) {
+    console.log(response);
+    var cityID = response.results[0].id;
+    $.ajax({
+      url: "https://api.predicthq.com/v1/events?place.scope=" + cityID + "&category=sports",
+      method: "GET",
+      headers: {
+        "Authorization": "Bearer TV3nvxQ22JQnqlWiIJzMa3wvh5wRBwCwMHUqdueb"
+      }
+    }).then(function (response) {
+      console.log(response);
+      
+    });
+  });
